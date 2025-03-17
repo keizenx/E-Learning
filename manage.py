18fe056573895da34bbf1ef0cb4d57010b237c82
samuel kouassi <kouassisamuelo226@gmail.com>
2025-03-17 11:42:00 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""
+Utilitaire de ligne de commande pour les tâches administratives de Django.
+"""
+
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """
+    Exécute les tâches administratives de Django.
+    """
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_management.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Impossible d'importer Django. Vérifiez qu'il est bien installé et "
+            "disponible dans votre variable d'environnement PYTHONPATH. "
+            "Avez-vous activé votre environnement virtuel ?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
