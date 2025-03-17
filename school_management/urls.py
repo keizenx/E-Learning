45265f361032e6b0ef8ce70a41a137a219b25eba
Y-creator249 <yanncedricemmanuelo@gmail.com>
2025-03-17 01:26:44 +0100

    Update urls.py

--- school_management/urls.py
+++ school_management/urls.py
@@ -18,8 +18,55 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
+from django.contrib.auth.models import User
+from School.models import Utilisateur
+from rest_framework import routers, serializers, viewsets
+from School.viewsets import (
+    StructureAcademiqueViewSet, FiliereViewSet, ClasseViewSet, UtilisateurViewSet, 
+    DiscussionViewSet, MessageViewSet, ForumViewSet, QuizViewSet, QuestionViewSet, 
+    CoursViewSet, MaterielViewSet, EmploiDuTempsViewSet, ResultatQuizViewSet
+)
+from rest_framework.permissions import IsAuthenticated
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Configuration du routeur DRF
+router = routers.DefaultRouter()
+router.register(r'structures', StructureAcademiqueViewSet)
+router.register(r'filieres', FiliereViewSet)
+router.register(r'classes', ClasseViewSet)
+router.register(r'utilisateurs', UtilisateurViewSet)
+router.register(r'discussions', DiscussionViewSet)
+router.register(r'messages', MessageViewSet)
+router.register(r'forums', ForumViewSet)
+router.register(r'quizzes', QuizViewSet)
+router.register(r'questions', QuestionViewSet)
+router.register(r'cours', CoursViewSet)
+router.register(r'materiels', MaterielViewSet)
+router.register(r'emplois-du-temps', EmploiDuTempsViewSet)
+router.register(r'resultats-quizz', ResultatQuizViewSet)
+
+# Vue pour générer la documentation Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API School Management",
+        default_version='v1',
+        description="Documentation de l'API pour la gestion scolaire",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@school.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(IsAuthenticated,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('School.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
